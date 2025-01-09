70b12664236eb3bf5b9d77666f6e61a5a4d0f444
felipe-u <felipe_uv@outlook.com>
2025-01-08 21:40:06 -0500

    Creación del mapa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,23 @@
 import { Component, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { GoogleMap } from '@angular/google-maps';
 
 import { LocationsService } from './services/locations.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [GoogleMap],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   private locationsService = inject(LocationsService);
 
-  locations$ = this.locationsService.getAllLocations({ lat: 10.9845951, lng: -74.8179751 });
+  center = signal<google.maps.LatLngLiteral>({ lat: 10.9845951, lng: -74.8179751 });
+  zoom = signal(4);
+
+  locations$ = this.locationsService.getAllLocations(this.center());
   $locations = toSignal(this.locations$, {
     initialValue: [],
   });
