bfc67e93b4e40185e27b8bec3cd93fc7aa677c46
felipe-u <felipe_uv@outlook.com>
2025-01-09 21:35:48 -0500

    Añadir marcadores avanzados y ajustar configuración del mapa en app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { GoogleMap } from '@angular/google-maps';
+import { GoogleMap, MapAdvancedMarker } from '@angular/google-maps';
 
 import { LocationsService } from './services/locations.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [GoogleMap],
+  imports: [GoogleMap, MapAdvancedMarker],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -15,7 +15,9 @@ export class AppComponent {
   private locationsService = inject(LocationsService);
 
   center = signal<google.maps.LatLngLiteral>({ lat: 10.9845951, lng: -74.8179751 });
-  zoom = signal(4);
+  zoom = signal(12);
+  // advancedMarkerOptions = signal<google.maps.marker.AdvancedMarkerElementOptions>({ gmpDraggable: false })
+  // advancedMarkerPositions = signal<google.maps.LatLngLiteral[]>([]);
 
   locations$ = this.locationsService.getAllLocations(this.center());
   $locations = toSignal(this.locations$, {
